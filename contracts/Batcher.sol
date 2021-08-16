b4a562ae3d2c2c5a423643f2901bf9707447ac3f
Tobias <tobias.eth@protonmail.com>
2021-08-16 10:39:13 -0300

    feat: batch it

--- contracts/Batcher.sol
+++ contracts/Batcher.sol
@@ -3,8 +3,11 @@ pragma solidity ^0.8.4;
 
 import 'hardhat/console.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 
 contract Batcher {
+  using SafeERC20 for ERC20;
+
   constructor(ERC20 _depositToken, ERC20 _withdrawToken) {
     depositToken = _depositToken;
     withdrawToken = _withdrawToken;
@@ -14,10 +17,49 @@ contract Batcher {
 
   ERC20 public withdrawToken;
 
-  mapping(address => uint256) public usersDeposits;
+  mapping(address => uint256) private _usersDeposits;
+
+  mapping(address => uint256) private _withdrawBalances;
+
+  address[] public depositors;
 
   function deposit(uint256 amount) public {
-    depositToken.transferFrom(msg.sender, address(this), amount);
-    usersDeposits[msg.sender] += amount;
+    if (_usersDeposits[msg.sender] == 0) {
+      depositors.push(msg.sender);
+    }
+    depositToken.safeTransferFrom(msg.sender, address(this), amount);
+    _usersDeposits[msg.sender] += amount;
+  }
+
+  function withdrawDeposited(uint256 amount) public {
+    require(_usersDeposits[msg.sender] >= amount, 'withdraw amount exceeds deposit');
+    depositToken.safeTransfer(msg.sender, amount);
+    _usersDeposits[msg.sender] -= amount;
+  }
+
+  function withdraw(uint256 amount) public {
+    require(_withdrawBalances[msg.sender] >= amount, 'withdraw amount too high');
+    withdrawToken.safeTransfer(msg.sender, amount);
+    _withdrawBalances[msg.sender] -= amount;
+  }
+
+  function _batchAction() private pure returns (bool) {
+    //Deposit, withdraw, whatever
+    return true;
+  }
+
+  function batch() public {
+    uint256 depositBalance = depositToken.balanceOf(address(this));
+    uint256 prevWithdrawBalance = withdrawToken.balanceOf(address(this));
+    _batchAction();
+    uint256 newWithdrawBalance = withdrawToken.balanceOf(address(this)) - prevWithdrawBalance;
+
+    for (uint256 i = 0; i < depositors.length; i++) {
+      address depositor = depositors[i];
+      uint256 depositorDeposit = _usersDeposits[depositor];
+      _withdrawBalances[depositor] += (depositorDeposit * newWithdrawBalance) / depositBalance;
+      delete _usersDeposits[depositor];
+    }
+    delete depositors;
   }
 }
